ad1499fa48f7132152ed75946c753c5215a0495c
Vedant Zope <vedantzope9@gmail.com>
2025-07-29 01:15:35 +0530

    Initial Setup

---
+++ src/main/java/com/zerodha/service/ZerodhaServiceApplication.java
@@ -0,0 +1,12 @@
+package com.zerodha.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerodhaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerodhaServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zerodha/service/ZerodhaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerodha.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerodhaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
