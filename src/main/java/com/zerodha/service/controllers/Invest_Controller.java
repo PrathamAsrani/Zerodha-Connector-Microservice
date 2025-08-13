2a5da6b71b5be00ecf552874d5cb9f8a6c2a7a67
Pratham Asrani <PrathamAsrani.cs@gmail.com>
2025-08-13 14:35:57 +0530

    feats: get orders
    -- created a route to get the user orders
    -- tested the whole flow for placing orders
    -- tested getting orders
    -- updated logic to save price from zerodha entity(order) in case of
    market order

--- src/main/java/com/zerodha/service/controllers/Invest_Controller.java
+++ src/main/java/com/zerodha/service/controllers/Invest_Controller.java
@@ -2,14 +2,14 @@ package com.zerodha.service.controllers;
 
 import com.zerodha.service.model.dtos.OrderRequestDto;
 import com.zerodha.service.model.dtos.OrderResponseDto;
+import com.zerodha.service.services.OrderService;
 import com.zerodha.service.services.ZerodhaInvestService;
 import com.zerodha.service.services.ZerodhaUserService;
+import com.zerodhatech.models.Order;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -17,14 +17,26 @@ import java.util.concurrent.CompletableFuture;
 @RequestMapping("/invest")
 public class Invest_Controller {
     private ZerodhaInvestService zerodhaInvestService;
+    private OrderService orderService;
 
-    public Invest_Controller(ZerodhaInvestService zerodhaInvestService){
+    public Invest_Controller(ZerodhaInvestService zerodhaInvestService, OrderService orderService){
         this.zerodhaInvestService=zerodhaInvestService;
+        this.orderService = orderService;
     }
 
-    @PostMapping("/place-order")
+    @PostMapping(
+            value = "/place-order",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     @Async
-    public CompletableFuture<ResponseEntity<Object>> placeOrder(@RequestBody OrderRequestDto orderRequestDto){
+    public CompletableFuture<ResponseEntity<OrderResponseDto>> placeOrder(@RequestBody OrderRequestDto orderRequestDto){
         return CompletableFuture.completedFuture(zerodhaInvestService.placeOrder(orderRequestDto));
+    }
+
+    @GetMapping("/get-orders")
+    @Async
+    public CompletableFuture<ResponseEntity<Object>> getOrders(){
+        return CompletableFuture.completedFuture(orderService.getOrders());
     }
 }
