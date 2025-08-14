2fcf22b36f94e860e8d9cf89a48439512b4fcc5a
Vedant Zope <vedantzope9@gmail.com>
2025-08-14 18:37:45 +0530

    Added getters and setters to DTO and Entity

--- src/main/java/com/zerodha/service/model/OrderEntity.java
+++ src/main/java/com/zerodha/service/model/OrderEntity.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Id;
 import lombok.*;
 
 import java.time.Instant;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -40,6 +41,142 @@ public class OrderEntity {
     @Enumerated(EnumType.STRING)
     private ValidityEnum validity;
 
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getTradingsymbol() {
+        return tradingsymbol;
+    }
+
+    public void setTradingsymbol(String tradingsymbol) {
+        this.tradingsymbol = tradingsymbol;
+    }
+
+    public ExchangeEnum getExchange() {
+        return exchange;
+    }
+
+    public void setExchange(ExchangeEnum exchange) {
+        this.exchange = exchange;
+    }
+
+    public TransactionEnum getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(TransactionEnum transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public OrderEnum getOrderType() {
+        return orderType;
+    }
+
+    public void setOrderType(OrderEnum orderType) {
+        this.orderType = orderType;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public ProductEnum getProduct() {
+        return product;
+    }
+
+    public void setProduct(ProductEnum product) {
+        this.product = product;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getTriggerPrice() {
+        return triggerPrice;
+    }
+
+    public void setTriggerPrice(double triggerPrice) {
+        this.triggerPrice = triggerPrice;
+    }
+
+    public int getDisclosedQuantity() {
+        return disclosedQuantity;
+    }
+
+    public void setDisclosedQuantity(int disclosedQuantity) {
+        this.disclosedQuantity = disclosedQuantity;
+    }
+
+    public ValidityEnum getValidity() {
+        return validity;
+    }
+
+    public void setValidity(ValidityEnum validity) {
+        this.validity = validity;
+    }
+
+    public Double getSquareoff() {
+        return squareoff;
+    }
+
+    public void setSquareoff(Double squareoff) {
+        this.squareoff = squareoff;
+    }
+
+    public Double getStoploss() {
+        return stoploss;
+    }
+
+    public void setStoploss(Double stoploss) {
+        this.stoploss = stoploss;
+    }
+
+    public Double getTrailingStoploss() {
+        return trailingStoploss;
+    }
+
+    public void setTrailingStoploss(Double trailingStoploss) {
+        this.trailingStoploss = trailingStoploss;
+    }
+
+    public VarietyEnum getVariety() {
+        return variety;
+    }
+
+    public void setVariety(VarietyEnum variety) {
+        this.variety = variety;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public Instant getPlacedAt() {
+        return placedAt;
+    }
+
+    public void setPlacedAt(Instant placedAt) {
+        this.placedAt = placedAt;
+    }
+
     private Double squareoff;
     private Double stoploss;
     private Double trailingStoploss;
@@ -50,4 +187,6 @@ public class OrderEntity {
     private String tag;
 
     private Instant placedAt;
+
+
 }
--- src/main/java/com/zerodha/service/model/dtos/OrderRequestDto.java
+++ src/main/java/com/zerodha/service/model/dtos/OrderRequestDto.java
@@ -24,4 +24,124 @@ public class OrderRequestDto {
     public Double trailingStoploss;
     public VarietyEnum variety;
     public String tag;
+
+    public String getTradingsymbol() {
+        return tradingsymbol;
+    }
+
+    public void setTradingsymbol(String tradingsymbol) {
+        this.tradingsymbol = tradingsymbol;
+    }
+
+    public ExchangeEnum getExchange() {
+        return exchange;
+    }
+
+    public void setExchange(ExchangeEnum exchange) {
+        this.exchange = exchange;
+    }
+
+    public TransactionEnum getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(TransactionEnum transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public OrderEnum getOrderType() {
+        return orderType;
+    }
+
+    public void setOrderType(OrderEnum orderType) {
+        this.orderType = orderType;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public ProductEnum getProduct() {
+        return product;
+    }
+
+    public void setProduct(ProductEnum product) {
+        this.product = product;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getTriggerPrice() {
+        return triggerPrice;
+    }
+
+    public void setTriggerPrice(double triggerPrice) {
+        this.triggerPrice = triggerPrice;
+    }
+
+    public int getDisclosedQuantity() {
+        return disclosedQuantity;
+    }
+
+    public void setDisclosedQuantity(int disclosedQuantity) {
+        this.disclosedQuantity = disclosedQuantity;
+    }
+
+    public ValidityEnum getValidity() {
+        return validity;
+    }
+
+    public void setValidity(ValidityEnum validity) {
+        this.validity = validity;
+    }
+
+    public Double getSquareoffValue() {
+        return squareoffValue;
+    }
+
+    public void setSquareoffValue(Double squareoffValue) {
+        this.squareoffValue = squareoffValue;
+    }
+
+    public Double getStoplossValue() {
+        return stoplossValue;
+    }
+
+    public void setStoplossValue(Double stoplossValue) {
+        this.stoplossValue = stoplossValue;
+    }
+
+    public Double getTrailingStoploss() {
+        return trailingStoploss;
+    }
+
+    public void setTrailingStoploss(Double trailingStoploss) {
+        this.trailingStoploss = trailingStoploss;
+    }
+
+    public VarietyEnum getVariety() {
+        return variety;
+    }
+
+    public void setVariety(VarietyEnum variety) {
+        this.variety = variety;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
 }
